2eaf3f252676c3d80d45870d4ae8ecbb6a4d3d35
lobatskiy-repo <lobatskiy31@gmail.com>
2024-07-14 14:17:26 +0300

    update: draving data

---
+++ js/graphics.js
@@ -0,0 +1,10 @@
+const graphics = {};
+
+graphics.drawPoint = function (ctx, loc, color = "black", size = 8) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+
+  ctx.arc(...loc, size / 2, 0, Math.PI * 2);
+
+  ctx.fill();
+};
--- js/math.js
+++ js/math.js
@@ -7,6 +7,24 @@ math.invlerp = (a, b, v) => (v - a) / (b - a);
 math.remap = (oldA, oldB, newA, newB, s) =>
   math.lerp(newA, newB, math.invlerp(oldA, oldB, s));
 
+math.remapPoint = (oldBounds, newBounds, point) => {
+  return [
+    math.remap(
+      oldBounds.left,
+      oldBounds.right,
+      newBounds.left,
+      newBounds.right,
+      point[0]
+    ),
+    math.remap(
+      oldBounds.top,
+      oldBounds.bottom,
+      newBounds.top,
+      newBounds.bottom,
+      point[1]
+    ),
+  ];
+};
 math.formatNumber = (number, decimals = 0) => {
   return number.toFixed(decimals);
 };
